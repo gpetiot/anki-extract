6cc188edc308dad89d426cb9a66af3d0308f0fad
Guillaume Petiot <hello@guillaumepetiot.com>
2025-07-11 21:51:11 +0800

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'anki-extract'
-copyright = '2025, Guillaume Petiot'
-author = 'Guillaume Petiot'
-release = '0.1.0'
+project = "anki-extract"
+copyright = "2025, Guillaume Petiot"
+author = "Guillaume Petiot"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
